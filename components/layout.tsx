628e268406f6a39a032f0866bd6089176d3eb041
Bob Karemera Shema <bobshema14@gmail.com>
2025-06-12 12:13:06 +0200

    Setup commit

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import { Work_Sans } from "next/font/google";
+import Navbar from "./navbar";
+
+const workSans = Work_Sans({
+    subsets: ['latin']
+})
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className={workSans.className}>
+            <body>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,28 @@
+const navLinks = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'Jobs',
+        link: '/jobs'
+    },
+    {
+        name: 'Post a Job',
+        link: '/post-a-job'
+    },
+    {
+        name: 'Companies',
+        link: '/companies'
+    }
+]
+
+const Navbar = () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar
